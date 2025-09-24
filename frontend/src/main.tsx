411069e7c003c4a8117a86f35daac8636ec7d176
stlvce <egorglv@mail.ru>
2025-09-24 10:17:37 +0500

    strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <App />,
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
