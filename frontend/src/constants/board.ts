febddeb625b66ff6984a93041d02dd89ff1c1c71
stlvce <egorglv@mail.ru>
2025-10-02 01:46:10 +0500

    board 300x300

--- frontend/src/constants/board.ts
+++ frontend/src/constants/board.ts
@@ -1,5 +1,5 @@
-export const BOARD_WIDTH = 400;
-export const BOARD_HEIGHT = 400;
+export const BOARD_WIDTH = 300;
+export const BOARD_HEIGHT = 300;
 export const BG_WIDTH = 150;
 export const BG_HEIGHT = 150;
 export const CLICK_THRESHOLD = 5; // макс. расстояние (px), чтобы считалось кликом
